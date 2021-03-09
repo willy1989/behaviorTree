34dfa94e5f46454216c10c19eb936de0227b16de
Willy <willy.winelover@gmail.com>
2021-03-09 10:37:37 +0100

    Created the base Node class

---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Node : MonoBehaviour
+{
+    private Node parentNode;
+
+    [SerializeField] private NodeStatus status;
+
+    public abstract void doAction();
+
+    public abstract void checkStatus();
+
+    public abstract void directFlow();
+
+    public abstract void resetNode();
+}
